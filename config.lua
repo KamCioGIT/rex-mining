ce93c9d5e040d3e9422ac53f899fa242f07e73d1
RexshackGaming <rexshackuk@gmail.com>
2024-07-02 07:02:43 +0100

    change cronjob to every hour

--- config.lua
+++ config.lua
@@ -21,5 +21,5 @@ Config.CopperRockProp    = 'mp_sca_rock_grp_l_03' -- rock prop copperore
 Config.IronRockProp      = 'mp_sca_rock_grp_l_03' -- rock prop ironore
 
 -- cronjob
-Config.MiningCronJob = '*/1 * * * *' -- cronjob time (every hour = 0 * * * *) / (every 30 mins = */30 * * * *)
+Config.MiningCronJob = '0 * * * *' -- cronjob time (every hour = 0 * * * *) / (every 30 mins = */30 * * * *)
 Config.ServerNotify  = true
